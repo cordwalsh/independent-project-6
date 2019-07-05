2e5d80dfae8aac8fe50827562a1c6eccd7ebd808
cord walsh <cordwalsh@MacBook-Pro-3.local>
2019-07-05 12:28:01 -0700

    continue setting upfront end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // front end logic goes here
 import { DoctorList } from './doctor.js';
 import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#symptom-form').submit(function(event) {
+    event.preventDefault();
+
+  })
+})
