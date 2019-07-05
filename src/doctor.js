a14da8fc5986b049da9084f86da77cf35e6e7098
cord walsh <cordwalsh@MacBook-Pro-3.local>
2019-07-05 10:52:54 -0700

    continued setting up back end, setting up front end

--- src/doctor.js
+++ src/doctor.js
@@ -1,12 +1,18 @@
 // backend logic goes here
 class DoctorList {
-  getDoctorInfo(word) {
+  apiKey(symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=073a0ab9278f00a646eed43d0fd56f7d';
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=073a0ab9278f00a646eed43d0fd56f7d';
       request.onload = function () {
-        
-      }
-});
-}
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(error(request.statusText));
+        }
+      };
+      request.open('GET', url, true);
+      request.send();
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 // front end logic goes here
+import { DoctorList } from './doctor.js';
+import $ from 'jquery';
